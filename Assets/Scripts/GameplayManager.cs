cd41e789876a4b1255ee4ce3a8efbe27f5a4263b
Haoan <haoanluo@usc.edu>
2023-06-02 23:13:12 -0700

    tmp and timer

---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    public TextMeshProUGUI textMesh;
+    public float maxTime = 45.0f;
+    private float timer = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        float timeLeft = maxTime - timer;
+        textMesh.text = Mathf.Round(timeLeft).ToString()+"s";
+        if (timeLeft <= 0)
+        {
+            Time.timeScale = 0.0f;
+        }
+    }
+}
