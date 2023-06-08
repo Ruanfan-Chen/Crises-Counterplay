075f9ec46b8c61889da359a7c3bee83ea893546f
xinqusc <xinq@usc.edu>
2023-06-08 00:16:19 -0700

    pre merge

--- Assets/Scripts/CharacterLaunch.cs
+++ Assets/Scripts/CharacterLaunch.cs
@@ -5,36 +5,25 @@ using UnityEngine;
 public class CharacterLaunch : MonoBehaviour
 {
     public GameObject projectilePrefab;
-    public GameObject gameplayManager;
-    private float startDelay = 2.0f;
-    private float attackInterval = 0.2f;
-    private float angleOfLaunch = 90.0f;
+    private float attackTimer;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("LaunchProjectile", startDelay, attackInterval);
+        attackTimer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        attackTimer -= Time.deltaTime;
     }
 
-    void LaunchProjectile()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation * Quaternion.Euler(0, 0, Random.Range(-angleOfLaunch / 2, angleOfLaunch / 2)));
-
-        //to organize enemies and projectiles
-        GameObject disposable = GameObject.Find("disposable");
-        if (!disposable)
+        if (attackTimer <= 0 && collision.GetComponent<DestroyOnHit>() && !GetComponent<Faction>().IsFriendly(collision.GetComponentInParent<Faction>()))
         {
-            disposable = new GameObject("disposable");
+            GetComponent<LaunchProjectile>().Launch(projectilePrefab, GetComponent<CharacterAttribute>().GetProjectileSpeed(), collision.transform.position);
+            attackTimer = GetComponent<CharacterAttribute>().GetAttackInterval();
         }
-        projectile.transform.SetParent(disposable.transform);
-
-        projectile.GetComponent<SpriteRenderer>().color = Color.white;
-        projectile.GetComponent<DestroyOutOfBounds>().gameplayManager = gameplayManager;
-        projectile.GetComponent<Faction>().SetHostility(false);
     }
 }
