a266c8c7a542baa47aa7737e9a43187f1b411fc0
xinqusc <xinq@usc.edu>
2023-06-10 21:57:22 -0700

    Update AimlesslyMove.cs

--- Assets/Scripts/AimlesslyMove.cs
+++ Assets/Scripts/AimlesslyMove.cs
@@ -6,11 +6,15 @@ public class AimlesslyMove : MonoBehaviour
 {
     private float timer;
     private Vector3 direction;
-    private float speed = 1.0f;
+    private float speed = 2.5f;
+    private float minHaltTime = 0.5f;
+    private float maxHaltTime = 1.5f;
+    private float minMoveDistance = 2.5f;
+    private float maxMoveDistance = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
-        Stop(0);
+        Halt(0);
     }
 
     // Update is called once per frame
@@ -23,13 +27,13 @@ public class AimlesslyMove : MonoBehaviour
             if (direction.magnitude == 0)
             {
                 float theta = Random.Range(0.0f, 2 * Mathf.PI);
-                Move(Random.Range(1.0f, 3.0f) * new Vector3(Mathf.Cos(theta), Mathf.Sin(theta), 0));
+                Move(Random.Range(minMoveDistance, maxMoveDistance) * new Vector3(Mathf.Cos(theta), Mathf.Sin(theta), 0));
             }
             else
-                Stop(Random.Range(0.5f, 1.5f));
+                Halt(Random.Range(minHaltTime, maxHaltTime));
         }
     }
-    void Stop(float seconds)
+    void Halt(float seconds)
     {
         timer = seconds;
         direction = Vector3.zero;
