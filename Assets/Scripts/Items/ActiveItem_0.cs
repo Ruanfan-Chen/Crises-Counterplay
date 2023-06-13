35611c3b56c5c0ff0ac78e204b0f2fe009651021
xinqusc <xinq@usc.edu>
2023-06-13 01:32:35 -0700

    Reworked Activeitem0

--- Assets/Scripts/Items/ActiveItem_0.cs
+++ Assets/Scripts/Items/ActiveItem_0.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class ActiveItem_0 : ActiveItem
 {
+    private int count = 6;
     public override void Activate()
     {
         Character character = GetComponent<Character>();
-        List<GameObject> ring = Projectile.InstantiateRing(transform.position, 0, 10);
-        foreach (GameObject projectile in ring)
+        float angleOfView=character.GetAngleOfView();
+        List<GameObject> projectiles = new();
+        for (float i = 0; i < count; i++)
+            projectiles.Add(Projectile.Instantiate(transform.position, Quaternion.Euler(0, 0, (i / (count - 1) - 0.5f) * angleOfView) * transform.rotation));
+        foreach (GameObject projectile in projectiles)
             foreach (IProjectileModifier modifier in GetComponents<IProjectileModifier>())
                 modifier.Modify(projectile);
     }
