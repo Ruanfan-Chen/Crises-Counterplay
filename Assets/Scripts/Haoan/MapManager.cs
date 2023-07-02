2a7b5e9db63798f98c05cfd249958a8e099a812b
xinqusc <97579117+xinqusc@users.noreply.github.com>
2023-07-01 18:07:07 -0700

    Split VechicleSpawn from MapManager

--- Assets/Scripts/Haoan/MapManager.cs
+++ Assets/Scripts/Haoan/MapManager.cs
@@ -20,15 +20,7 @@ public class MapManager : MonoBehaviour
     [SerializeField] private Sprite[] m_spriteArray;
     [SerializeField] private Color m_mapColor = new Color(0.3584906f, 0.3315237f, 0.2113742f);
     [SerializeField] private GameObject[] m_elementArray;
-    [SerializeField] private float m_startDelay;
     private SpriteRenderer m_spriteRenderer;
-
-    private float vehicleTimer = float.PositiveInfinity;
-
-    // event will occur within [m_event_intervl_min, m_event_intervl_max] after last occurance
-    public float m_event_intervl_min = 1.0f; // min time (in seconds) after last event occurred
-
-    public float m_event_intervl_max = 3.0f; // max time (in seconds) after last event occurred
 
     private int m_level;
 
@@ -47,22 +39,6 @@ public class MapManager : MonoBehaviour
         m_yLower = -m_heightNum * m_gridSize;
         m_yUpper = m_heightNum * m_gridSize;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        vehicleTimer -= Time.deltaTime;
-        if (vehicleTimer <= 0)
-        {
-            float length = 2.24f * 30.0f;
-            Vector3 startPos = GetRandomPointOnEdge();
-            Vector3 targetPos = startPos + (m_player.transform.position - startPos).normalized * length;
-
-            Vehicle.Instantiate(startPos, targetPos);
-            vehicleTimer = Random.Range(m_event_intervl_min, m_event_intervl_max);
-        }
-    }
-
 
     public void CreateMap(Shape shape)
     {
@@ -106,11 +82,7 @@ public class MapManager : MonoBehaviour
     public void LoadLevel(int levelNum)
     {
         m_level = levelNum;
-        // Update event interval; m_level >= 1
-        //m_event_intervl_max = Mathf.Max(1.0f, m_event_intervl_max - (m_level - 1.0f));
-        //m_event_intervl_min = Mathf.Max(1.0f, m_event_intervl_min - (m_level - 1.0f));
-        //Debug.Log("m_event_intervl_min = " + m_event_intervl_min + ", m_event_intervl_max = " + m_event_intervl_max);
-        vehicleTimer = Random.Range(m_event_intervl_min, m_event_intervl_max) + m_startDelay;
+
         if (levelNum == 1)
         {
             CreateMap(Shape.Rectangle);
