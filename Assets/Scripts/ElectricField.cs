15ab41e822a23fc189b8aacd47f9f20bfed0033c
xinqusc <97579117+xinqusc@users.noreply.github.com>
2023-07-01 18:46:54 -0700

    Electric field prefab and script created

---
+++ Assets/Scripts/ElectricField.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class ElectricField : MonoBehaviour
+{
+    private static string prefabPath = "Prefabs/ElectricField";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static GameObject Instantiate(Vector3 position, float radius, float duration) {
+        return null;
+    }
+}
