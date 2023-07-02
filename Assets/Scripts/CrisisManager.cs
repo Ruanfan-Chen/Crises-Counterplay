2376f5c961758e9eca917a45b4e01d3ed54b8b40
xinqusc <97579117+xinqusc@users.noreply.github.com>
2023-07-01 18:28:11 -0700

    Rename VehicleSpawn to CrisisManager

--- Assets/Scripts/CrisisManager.cs
+++ Assets/Scripts/CrisisManager.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VehicleSpawn : MonoBehaviour
+public class CrisisManager : MonoBehaviour
 {
     [SerializeField] private float m_startDelay;
     // event will occur within [m_event_intervl_min, m_event_intervl_max] after last occurance
     [SerializeField] private float m_event_intervl_min; // min time (in seconds) after last event occurred
     [SerializeField] private float m_event_intervl_max; // max time (in seconds) after last event occurred
     [SerializeField] private GameObject m_player;
-    private float vehicleTimer;
+    private float eventTimer;
 
     void Start()
     {
@@ -17,21 +17,34 @@ public class VehicleSpawn : MonoBehaviour
         //m_event_intervl_max = Mathf.Max(1.0f, m_event_intervl_max - (m_level - 1.0f));
         //m_event_intervl_min = Mathf.Max(1.0f, m_event_intervl_min - (m_level - 1.0f));
         //Debug.Log("m_event_intervl_min = " + m_event_intervl_min + ", m_event_intervl_max = " + m_event_intervl_max);
-        vehicleTimer = Random.Range(m_event_intervl_min, m_event_intervl_max) + m_startDelay;
+        eventTimer = Random.Range(m_event_intervl_min, m_event_intervl_max) + m_startDelay;
     }
 
     // Update is called once per frame
     void Update()
     {
-        vehicleTimer -= Time.deltaTime;
-        if (vehicleTimer <= 0)
+        eventTimer -= Time.deltaTime;
+        if (eventTimer <= 0)
         {
-            float length = 2.24f * 30.0f;
-            Vector3 startPos = GetComponent<MapManager>().GetRandomPointOnEdge();
-            Vector3 targetPos = startPos + (m_player.transform.position - startPos).normalized * length;
-
-            Vehicle.Instantiate(startPos, targetPos);
-            vehicleTimer = Random.Range(m_event_intervl_min, m_event_intervl_max);
+            SpawnVehicle();
+            eventTimer = Random.Range(m_event_intervl_min, m_event_intervl_max);
         }
+    }
+
+    void SpawnVehicle() {
+        float length = 2.24f * 30.0f;
+        Vector3 startPos = GetComponent<MapManager>().GetRandomPointOnEdge();
+        Vector3 targetPos = startPos + (m_player.transform.position - startPos).normalized * length;
+
+        Vehicle.Instantiate(startPos, targetPos);
+    }
+
+    void SpawnElectricField() {
+
+    }
+
+    void SpawnTidalWave()
+    {
+
     }
 }
