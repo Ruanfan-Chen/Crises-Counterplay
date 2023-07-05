0425e20079826293a3f73b166f4e2e2473778e14
xinqusc <97579117+xinqusc@users.noreply.github.com>
2023-07-05 09:30:26 -0700

    Terrain Bug fix

--- Assets/Scripts/Terrain.cs
+++ Assets/Scripts/Terrain.cs
@@ -5,61 +5,9 @@ using UnityEngine;
 
 public class Terrain : MonoBehaviour
 {
-    private GameObject NNeighbour;
-    private GameObject SNeighbour;
-    private GameObject NENeighbour;
-    private GameObject NWNeighbour;
-    private GameObject SENeighbour;
-    private GameObject SWNeighbour;
+    Vector2Int gridPos;
 
-    public GameObject GetNNeighbour() { return GetNNeighbour(); }
+    public Vector2Int GetGridPos() { return gridPos; }
 
-    public void SetNNeighbour(GameObject value) { SetNNeighbour(value); }
-
-    public GameObject GetSNeighbour() { return GetSNeighbour(); }
-
-    public void SetSNeighbour(GameObject value) { SetSNeighbour(value); }
-
-    public GameObject GetNENeighbour() { return GetNENeighbour(); }
-
-    public void SetNENeighbour(GameObject value) { SetNENeighbour(value); }
-
-    public GameObject GetNWNeighbour() { return GetNWNeighbour(); }
-
-    public void SetNWNeighbour(GameObject value) { SetNWNeighbour(value); }
-
-    public GameObject GetSENeighbour() { return GetSENeighbour(); }
-
-    public void SetSENeighbour(GameObject value) { SetSENeighbour(value); }
-
-    public GameObject GetSWNeighbour() { return GetSWNeighbour(); }
-
-    public void SetSWNeighbour(GameObject value) { SetSWNeighbour(value); }
-
-    public IEnumerable<GameObject> GetNeighbour()
-    {
-        return new HashSet<GameObject>() { NNeighbour, SNeighbour, NENeighbour, NWNeighbour, SENeighbour, SWNeighbour }.Where(gameObject => gameObject);
-    }
-
-    //public GameObject BFS()
-    //{
-    //    Queue<GameObject> queue = new();
-    //    queue.Enqueue(gameObject);
-    //    HashSet<GameObject> explored = new() { gameObject };
-    //    while (queue.Count > 0)
-    //    {
-    //        GameObject current = queue.Dequeue();
-    //        if (false)
-    //            return current;
-    //        foreach (GameObject neighbour in GetNeighbour())
-    //        {
-    //            if (!explored.Contains(neighbour))
-    //            {
-    //                explored.Add(neighbour);
-    //                queue.Enqueue(neighbour);
-    //            }
-    //        }
-    //    }
-    //    return null;
-    //}
+    public void SetGridPos(Vector2Int value) { gridPos = value; }
 }
--- Assets/Scripts/TerrainManager.cs
+++ Assets/Scripts/TerrainManager.cs
@@ -1,32 +1,73 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
+using static Utility;
 
 public class TerrainManager : MonoBehaviour
 {
     public static readonly float TERRAIN_DEPTH = 0.25f;
-    public static readonly float UNIT_SIZE = 3.0f;
+    public static readonly float UNIT_SIZE = 1.0f;
     private static readonly float sqrt3 = Mathf.Sqrt(3);
+    private BiDictionary<Vector2Int, GameObject> terrainGraph = new();
 
     public void Initialize(Bounds bounds)
     {
+        Queue<Vector2Int> queue = new();
+        queue.Enqueue(Vector2Int.zero);
+        HashSet<Vector2Int> explored = new() { Vector2Int.zero };
+        while (queue.Count > 0)
+        {
+            Vector2Int current = queue.Dequeue();
+            InstantiateUnit(current);
+            foreach (Vector2Int neighbour in GetNeighbour(current).Where(gridPos => IsInBounds(gridPos, bounds)))
+            {
+                if (!explored.Contains(neighbour))
+                {
+                    explored.Add(neighbour);
+                    queue.Enqueue(neighbour);
+                }
+            }
+        }
     }
 
-    private GameObject InstantiateUnit(Vector2Int girdPos)
+    private GameObject InstantiateUnit(Vector2Int gridPos)
     {
-        GameObject unit = new();
-        unit.transform.position = GetPosition3D(girdPos);
+        GameObject unit = new("Terrain" + gridPos.x + gridPos.y);
+        unit.transform.position = GetPosition3D(gridPos);
+        unit.AddComponent<Terrain>().SetGridPos(gridPos);
+        terrainGraph.Add(gridPos, unit);
         return unit;
     }
 
-    public static Vector2 GetPosition2D(Vector2Int girdPos)
+    public static IEnumerable<Vector2Int> GetNeighbour(Vector2Int gridPos)
     {
-        return new Vector2(girdPos.x * UNIT_SIZE * sqrt3 / 2.0f, girdPos.y * UNIT_SIZE + (x % 2) * sqrt3);
+        return new HashSet<Vector2Int>()
+        {
+            gridPos + Vector2Int.left * 2,
+            gridPos + Vector2Int.left,
+            gridPos + Vector2Int.right,
+            gridPos + Vector2Int.right * 2,
+            gridPos + Vector2Int.left - (gridPos.x - 1) % 2 * Vector2Int.up,
+            gridPos + Vector2Int.right - (gridPos.x + 1) % 2 * Vector2Int.up
+        };
     }
 
-    public static Vector3 GetPosition3D(Vector2Int girdPos)
+    public static Vector2 GetPosition2D(Vector2Int gridPos)
     {
-        Vector2 position2 = GetPosition2D(girdPos);
+        return new Vector2(gridPos.x * UNIT_SIZE * sqrt3 / 2.0f, (gridPos.y * 2 + gridPos.x % 2) * UNIT_SIZE );
+    }
+
+    public static Vector3 GetPosition3D(Vector2Int gridPos)
+    {
+        Vector2 position2 = GetPosition2D(gridPos);
         return new Vector3(position2.x, position2.y, TERRAIN_DEPTH);
+    }
+
+    public static bool IsInBounds(Vector2Int gridPos, Bounds bounds)
+    {
+        return bounds.SqrDistance(GetPosition2D(gridPos)) <= UNIT_SIZE * UNIT_SIZE;
     }
 }
