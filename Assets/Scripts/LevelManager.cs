1879f6a5e9907734312162727b38116f79e4eb0c
Ruanfan Chen <ruanfanchen@Israels-MBP.usc.edu>
2023-07-20 19:26:43 -0700

    Modified shop options for level 0

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -124,7 +124,7 @@ public class LevelManager
     {
         List<ShopOption> options = levelNum switch
         {
-            0 => new() { ShopOption.TRAINBOUND, ShopOption.CHISTRIKE },
+            0 => new() {ShopOption.CHISTRIKE },
             1 => new(),
             2 => new() { new ShopOption(UnityEngine.Random.Range(15, 36)), ShopOption.TOXICFOOTPRINT },
             3 => new() { ShopOption.SUPERCHARGE },
