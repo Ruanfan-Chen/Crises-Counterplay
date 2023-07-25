6cd6c4061b684d9d9273c5d7cc064ed86fd9cf0b
xinqusc <97579117+xinqusc@users.noreply.github.com>
2023-07-24 22:21:47 -0700

    Bomb Adjustment

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -4,6 +4,7 @@ using static Utility;
 public class Bomb : MonoBehaviour
 {
     private static readonly string prefabPath = "Prefabs/Bomb";
+    private static readonly string explosionPrefabPath = "Prefabs/Explosion";
     private readonly float damage = 100.0f;
     private readonly float explosionDelay = 3.0f;
     private float timer;
@@ -26,6 +27,11 @@ public class Bomb : MonoBehaviour
         timer -= Time.deltaTime;
         if (timer <= 0)
         {
+            GameObject explosion = Instantiate(Resources.Load<GameObject>(explosionPrefabPath), transform.position + Vector3.forward * MapManager.MAP_DEPTH / 2.0f, Quaternion.identity);
+            Destroy(explosion, 0.5f);
+            float radius = GetComponent<CircleCollider2D>().radius;
+            explosion.transform.localScale = new Vector3(radius * 2.0f, radius * 2.0f, 1.0f);
+            explosion.tag = "Disposable";
             foreach (GameObject g in OverlapGameObject(gameObject, collision => collision.GetComponent<IDamageable>() != null))
             {
                 IDamageable damageable = g.GetComponent<IDamageable>();
